d03cbba6cc73a34048976d6fb9d144f662862ef9
daniy <daniy@DESKTOP-7F73REQ>
2022-11-15 18:26:54 +0500

    Добавьте файлы проекта.

---
+++ HelloWorldConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace HelloWorldConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
